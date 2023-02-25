0e2f4f308db676a902e064f0441405b2e9ded482
naoki-ando <naoki.ando@geniee.co.jp>
2023-02-26 00:15:48 +0900

    test

--- redis_test.go
+++ redis_test.go
@@ -20,3 +20,25 @@ func TestRedis(t *testing.T) {
 	}
 	fmt.Println(pong)
 }
+
+func TestRedisCluster(t *testing.T) {
+	client := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{"localhost:7001", "localhost:7002", "localhost:7003", "localhost:7004", "localhost:7005", "localhost:7006"},
+	})
+	ctx := context.Background()
+	err := client.ForEachShard(ctx, func(ctx context.Context, client *redis.Client) error {
+		pong, err := client.Ping(ctx).Result()
+		if err != nil {
+			return err
+		}
+		fmt.Println(pong)
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = client.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
